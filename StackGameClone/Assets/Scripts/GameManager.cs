18410de4aae5af2c5cdd65744dacec6f3664e5e7
RedLegend97 <alpermisirli@gmail.com>
2021-09-22 12:37:34 +0300

    Added camera movement

--- StackGameClone/Assets/Scripts/GameManager.cs
+++ StackGameClone/Assets/Scripts/GameManager.cs
@@ -7,14 +7,21 @@ using TouchPhase = UnityEngine.InputSystem.TouchPhase;
 public class GameManager : MonoBehaviour
 {
     public static event Action OnCubeSpawn = delegate { };
-
+    private Camera m_MainCamera;
     private CubeSpawner[] spawners;
     private int spawnerIndex;
     private CubeSpawner currentSpawner;
+    [SerializeField] private float camSpeed;
 
     private void Awake()
     {
         spawners = FindObjectsOfType<CubeSpawner>();
+    }
+
+    private void Start()
+    {
+        //This gets the Main Camera from the Scene
+        m_MainCamera = Camera.main;
     }
 
     private void Update()
@@ -27,6 +34,9 @@ public class GameManager : MonoBehaviour
                 MovingCube.CurrentCube.Stop();
             }
 
+            m_MainCamera.transform.position = Vector3.MoveTowards(m_MainCamera.transform.position, new Vector3(
+                m_MainCamera.transform.position.x,
+                m_MainCamera.transform.position.y + 0.18f, m_MainCamera.transform.position.z), camSpeed * Time.deltaTime);
             // FindObjectOfType<CubeSpawner>().SpawnCube();
             spawnerIndex = spawnerIndex == 0 ? 1 : 0;
 
